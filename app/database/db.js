3d9f190a8d19b755ce0fb99b80c1b311a95d74ff
nicolas.delafuente <dev@mobilityps.com>
2022-07-17 16:14:01 -0300

    Initial commit

---
+++ app/database/db.js
@@ -0,0 +1,12 @@
+const { Sequelize } = require('sequelize');
+const config = require('../../config/database');
+const db = {};
+
+db.connection = new Sequelize(
+                      config.database,
+                      config.username,
+                      config.password,
+                      config
+                    );
+
+module.exports = db;
---
+++ config/database.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const DATABASE  = process.env.DB_NAME
+const USERNAME  = process.env.DB_USERNAME
+const PASSWORD  = process.env.DB_PASSWORD
+const HOST      = process.env.DB_HOST
+const DIALECT   = process.env.DB_DIALECT
+
+
+module.exports = {
+  username: USERNAME,
+  password: PASSWORD,
+  database: DATABASE,
+  host: HOST,
+  dialect: DIALECT,
+  define: {
+    charset: 'utf8',
+    collate: 'utf8_general_ci', 
+    timestamps: false,
+    // Genera claves foraneas de este tipo persona_id en vez de userId
+    undescored: true,
+  }
+}
