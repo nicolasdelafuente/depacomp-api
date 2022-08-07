f8c1469ea06154e8cd89fc2a560cbb132a195cd8
nicolas.delafuente <dev@mobilityps.com>
2022-08-07 14:46:12 -0300

    Cambios en cors ver .env.example

--- app/server.js
+++ app/server.js
@@ -5,9 +5,14 @@ const routes = require('./routes');
 const cors = require('cors')
 
 // Setting
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 
-app.use(cors());
+const whiteList = process.env.WHITELIST;
+
+app.use(cors({
+    "Access-Control-Allow-Origin": whiteList,
+    "optionsSuccessStatus": 200
+}));
 // Middleware
 // Para poder rellenar el req.body
 app.use(express.json());
@@ -21,6 +26,6 @@ app.use("/depacomp-api/v1",routes);
 app.listen(PORT, function () {
     console.log(`La app ha arrancado en http://localhost:${PORT}`);
     connection.sync({ force: false }).then(() => {
-        console.log("Se ha establecido la conexión");
+        console.log(`Se ha establecido la conexión.`);
     })
 });
