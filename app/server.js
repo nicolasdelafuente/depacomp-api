ced761ff1bde76ab9300dc004641e87593b88376
nicolas.delafuente <dev@mobilityps.com>
2022-08-14 10:56:25 -0300

    Agregué la ruta en .env

--- app/server.js
+++ app/server.js
@@ -6,22 +6,22 @@ const cors = require('cors')
 
 // Setting
 const PORT = process.env.PORT || 4000;
-
-app.use(cors());
-const whiteList = process.env.WHITELIST;
+const ROUTE = process.env.Route
+const WHITELIST = process.env.WHITELIST;
 
 app.use(cors({
-    "Access-Control-Allow-Origin": whiteList,
+    "Access-Control-Allow-Origin": WHITELIST,
     "optionsSuccessStatus": 200
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Rutas
-app.use("/depacomp-api/v1",routes);
+
+app.use(ROUTE,routes);
 
 // Arrancamos el servidor
-// true dropea todo
+// TRUE dropea todo
 app.listen(PORT, function () {
     console.log(`La app ha arrancado en http://localhost:${PORT}`);
     connection.sync({ force: false }).then(() => {
