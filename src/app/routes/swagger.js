2f480a8ab4afb9b9252ede97d3ec6a7e02164463
Adrián Fazio <adrian.fazio@estudiantes.unahur.edu.ar>
2023-02-22 09:20:51 -0300

    swagger

--- src/app/routes/swagger.js
+++ src/app/routes/swagger.js
@@ -1,10 +1,11 @@
 const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require('swagger-ui-express');
+//para usar todas las rutas para swagger
+const path = require('path');
 
 const ROUTE         = process.env.ROUTE;
 const ROUTEVERSION  = process.env.ROUTEVERSION || '/v1';
-const path = `src/app/routes${ROUTEVERSION}`;
-console.log(path);
+//const path = `src/app/routes${ROUTEVERSION}`;
 
 const options = {
     definition: {
@@ -21,30 +22,23 @@ const options = {
         ]
         */
     },
-    apis: [
-        `${path}/carreras.routes.js`,
-    ]
+    apis: [`${path.join(__dirname, "./v1/*.js")}`],
+    //apis: [`${path}/carreras.routes.js`,]
 };
 
 
 const swaggerSpec = swaggerJSDoc(options);
 
 const swaggerDocs = (app, port) => {
-
-    app.use(
-        `./src/app/routes/`,
-        swaggerUi.serve,
-        swaggerUi.setup(swaggerSpec)
-    );
-
-    app.get(`./src/app/routes//docs.json`, (req,res) => {
-        res.header('Content-Type', 'application/json');
+    app.use(`${ROUTE}/docs`, swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.get(`${ROUTE}/docs.json`, (req,res) => {
+        res.setHeader('Content-Type', 'application/json');
         res.send(swaggerSpec);
     });
 
-    console.log(`Version 1 Docs at http://localhost:${port}${ROUTE}/doc.js`);
+    console.log(`Version 1 Docs at http://localhost:${port}${ROUTE}/docs`);
 };
 
-module.exports = {swaggerDocs}
+module.exports = { swaggerDocs }
 
 
