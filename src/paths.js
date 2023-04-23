92cab5988fd3b6abf661900e0daf79f17812d316
nicolas.delafuente <dev@mobilityps.com>
2023-04-23 11:24:35 -0300

    ADD: rutas para path

---
+++ src/paths.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  APP: path.join(__dirname, 'app'),
+  CONTROLLERS: path.join(__dirname, 'app', 'controllers'),
+  DATABASE: path.join(__dirname, 'app', 'database'),
+  MODELS: path.join(__dirname, 'app', 'models'),
+  ROUTES: path.join(__dirname, 'app', 'routes'),
+  CONFIG: path.join(__dirname, 'config')
+};
