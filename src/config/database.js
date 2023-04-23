e363e7ef950b43fd9b7ba7b662d125602f63ee32
nicolas.delafuente <dev@mobilityps.com>
2023-04-23 11:03:23 -0300

    ADD: try catch

--- src/config/database.js
+++ src/config/database.js
@@ -1,30 +1,32 @@
 require('dotenv').config();
 
-const DATABASE  = process.env.DB_NAME
-const USERNAME  = process.env.DB_USERNAME
-const PASSWORD  = process.env.DB_PASSWORD
-const HOST      = process.env.DB_HOST
-const DIALECT   = process.env.DB_DIALECT
+const DATABASE = process.env.DB_NAME;
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+const HOST = process.env.DB_HOST;
+const DIALECT = process.env.DB_DIALECT;
 
+try {
+  module.exports = {
+    username: USERNAME,
+    password: PASSWORD,
+    database: DATABASE,
+    host: HOST,
+    dialect: DIALECT,
 
-module.exports = {
-  username: USERNAME,
-  password: PASSWORD,
-  database: DATABASE,
-  host: HOST,
-  dialect: DIALECT,
-
-  define: {
-    charset: 'utf8',
-    collate: 'utf8_general_ci', 
-    // timestamps: false,
+    define: {
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+       // timestamps: false,
     // Genera claves foraneas de este tipo persona_id en vez de userId
-    underscored: true,
-  },
-  timezone: '-03:00',
-  dialectOptions: {
-    //useUTC: false, //for reading from database
-    dateStrings: true,
-    typeCast: true
-},
+      underscored: true,
+    },
+    timezone: '-03:00',
+    dialectOptions: {
+      dateStrings: true,
+      typeCast: true
+    },
+  }
+} catch (error) {
+  console.error(`Error: ${error.message}`);
 }
