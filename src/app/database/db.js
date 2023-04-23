5a944ae5136a75a3e9fb21a915dba64c2cbfc45a
nicolas.delafuente <dev@mobilityps.com>
2023-04-23 11:39:31 -0300

    CHANGE: urls paths

--- src/app/database/db.js
+++ src/app/database/db.js
@@ -1,58 +1,47 @@
+const path = require('../../paths');
+
+const fs = require('fs');
+const folderPath = '../models';
+
 const Sequelize = require("sequelize");
 
-const config = require('../../config/database');
+const config = require(`${path.CONFIG}/database`);
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-                    
+
 db.Sequelize = Sequelize;
-// Vinculación modelos a BD
 
-db.Carrera = require('../models/carrera')(sequelize, Sequelize);
-db.Categoria = require('../models/categoria')(sequelize, Sequelize);
-db.DocumentoTipo = require('../models/documentoTipo')(sequelize, Sequelize);
-db.Entrevista = require('../models/entrevista')(sequelize, Sequelize);
-db.Estado = require('../models/estado')(sequelize, Sequelize);
-db.Genero = require('../models/genero')(sequelize, Sequelize);
-db.Instituto = require('../models/instituto')(sequelize, Sequelize);
-db.Localidad = require('../models/localidad')(sequelize, Sequelize);
-db.Pais = require('../models/pais')(sequelize, Sequelize);
-db.Persona = require('../models/persona')(sequelize, Sequelize);
-db.Provincia = require('../models/provincia')(sequelize, Sequelize);
-db.Rol = require('../models/rol')(sequelize, Sequelize);
-db.Seguimiento = require('../models/seguimiento')(sequelize, Sequelize);
-db.SeguimientoTipo = require('../models/seguimientoTipo')(sequelize, Sequelize);
-db.Usuario = require('../models/usuario')(sequelize, Sequelize);
+// Obtengo un array con el nombre de los modelos.
+fs.readdir(folderPath, (err, files) => {
+  if (err) {
+    console.log(`Error al leer la carpeta: ${err}`);
+    return;
+  }
 
+  // Filtrar los archivos con extensión .js
+  const jsFiles = files.filter((file) => file.endsWith('.js'));
 
+  // Crear el array con los nombres de los archivos
+  const modelNames = jsFiles.map((file) => file.slice(0, -3));
 
-db.Carrera.associate(db);
-db.Categoria.associate(db);
-db.DocumentoTipo.associate(db);
-db.Entrevista.associate(db);
-db.Estado.associate(db);
-db.Genero.associate(db);
-db.Instituto.associate(db);
-db.Localidad.associate(db);
-db.Pais.associate(db);
-db.Persona.associate(db);
-db.Provincia.associate(db);
-db.Rol.associate(db);
-db.Seguimiento.associate(db);
-db.SeguimientoTipo.associate(db);
-db.Usuario.associate(db);
+  // Vinculación modelos a BD
+  modelNames.forEach(modelName => {
+    db[modelName] = require(`${path.MODELS}${modelName.toLowerCase()}`)(sequelize, Sequelize);
+    db[modelName].associate(db);
+  });
 
 
-/*
-db.Pais.hasMany(db.Persona, {
-  foreignKey: "pais_Id",
+  /*
+  db.Pais.hasMany(db.Persona, {
+    foreignKey: "pais_Id",
+  });
+  db.Persona.belongsTo(db.Pais, {
+    foreignKey: "pais_Id",
+  });
+  */
 });
-db.Persona.belongsTo(db.Pais, {
-  foreignKey: "pais_Id",
-});
-*/
-
 
 
 db.connection = sequelize
