a4f25a756feb6dbccb75090f91d33a63457cfed2
nicolas.delafuente <dev@mobilityps.com>
2023-05-25 20:21:07 -0300

    Modifico seeders

--- src/app/database/seeders/20220721191641-create-persona.js
+++ src/app/database/seeders/20220721191641-create-persona.js
@@ -4,22 +4,22 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
 
       let data = [
-        {  
+        {
           nombre: "nombre_1",
           apellido: "apellido_1",
           email: "nombre_apellido_1@gmail.com",
           password: "1234",
           documento: "",
           rol_id: 1,
-          //genero_id: 3,
-          //documento_id: 1,
-          //localidad_id: 1,
-          //provincia_id: 2,
-          //pais_id: 9,
+          genero_id: 3,
+          documento_id: 1,
+          localidad_id: 1,
+          provincia_id: 2,
+          pais_id: 9,
           created_at: new Date(),
           updated_at: new Date()
         },
-        {  
+        {
           nombre: "nombre_2",
           apellido: "apellido_2",
           email: "nombre_apellido_2@gmail.com",
@@ -34,7 +34,7 @@ module.exports = {
           created_at: new Date(),
           updated_at: new Date()
         },
-        {  
+        {
           nombre: "nombre_3",
           apellido: "apellido_3",
           email: "nombre_apellido_3@gmail.com",
@@ -49,7 +49,7 @@ module.exports = {
           created_at: new Date(),
           updated_at: new Date()
         },
-        {  
+        {
           nombre: "nombre_4",
           apellido: "apellido_4",
           email: "nombre_apellido_4@gmail.com",
@@ -64,7 +64,7 @@ module.exports = {
           created_at: new Date(),
           updated_at: new Date()
         },
-        {  
+        {
           nombre: "nombre_5",
           apellido: "apellido_5",
           email: "nombre_apellido_5@gmail.com",
@@ -79,7 +79,7 @@ module.exports = {
           created_at: new Date(),
           updated_at: new Date()
         },
-        {  
+        {
           nombre: "nombre_6",
           apellido: "apellido_6",
           email: "nombre_apellido_6@gmail.com",
@@ -102,6 +102,6 @@ module.exports = {
   down: (queryInterface, Sequelize) => {
 
       return queryInterface.bulkDelete('Personas', null, {});
-    
+
   }
 };
