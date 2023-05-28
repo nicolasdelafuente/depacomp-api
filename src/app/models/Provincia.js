887bc3cc04b075c6ab3c70c6e2df8b92bc787a1a
Yamil Amarillo <yamil.amarillo@unahur.edu.ar>
2023-05-28 13:15:45 -0300

    Refactorizacion de codigo minuscula por mayuscula -models- en provincia

--- src/app/models/Provincia.js
+++ src/app/models/Provincia.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
   Provincia.associate = function(models) {
     
     Provincia.belongsTo(models.Pais, {
-      as: "Pais",
+      as: "pais",
       foreignKey: "pais_id",
     });
 
