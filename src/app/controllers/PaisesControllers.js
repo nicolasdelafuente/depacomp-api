44409bead96079b23c305133a23db1a44e704128
Yamil Amarillo <yamil.amarillo@unahur.edu.ar>
2023-05-28 17:17:01 -0300

    Refactorizacion de codigo agregamos logger en PaisesControllers

--- src/app/controllers/PaisesControllers.js
+++ src/app/controllers/PaisesControllers.js
@@ -1,5 +1,8 @@
 const path = require('../../paths');
+const { handleErrors } = require(`${path.SERVICES}/logger`)
 const { Pais } = require(`${path.DATABASE}/db`);
+
+const nameController = "Pais"
 
 const attributes = [
   "id",
@@ -17,6 +20,7 @@ const create = async (req, res) => {
           data,
       });
   } catch (error) {
+      handleErrors(error, 'create', nameController);
       return res.status(500).json({ error: error.message })
   }
 }
@@ -29,7 +33,8 @@ const get = async (_, res) => {
 
     return res.status(200).json({ data });
     
-  } catch (error) {      
+  } catch (error) {  
+    handleErrors(error,'get', nameController);    
     return res.status(500).json({ error: error.message })
   }
 }
@@ -47,6 +52,7 @@ const getById = async (req, res) => {
       }
       return res.status(404).send({message: 'No existe Pais con el id especificado'});
   } catch (error) {
+      handleErrors(error, 'getById', nameController);
       return res.status(500).send({ error: error.message });
   }
 }
@@ -63,6 +69,7 @@ const update = async (req, res) => {
       }
       throw new Error('Pais not found');
   } catch (error) {
+      handleErrors(error, 'update', nameController);
       return res.status(500).send({ error: error.message });
   }
 };
@@ -78,6 +85,7 @@ const destroy = async (req, res) => {
       }
       throw new Error("Pais no encontrado");
   } catch (error) {
+      handleErrors(error, 'destroy', nameController);
       return res.status(500).send({ error: error.message });
   }
 };
