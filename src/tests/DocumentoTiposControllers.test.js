de32863d583c00c2fe0ed7b38bfafd44ea38dfc5
Adrián Fazio <adrian.fazio@estudiantes.unahur.edu.ar>
2023-05-30 09:39:18 -0300

    Subidos tests de DcoumentoTipos y SeguimientoTipos

---
+++ src/tests/DocumentoTiposControllers.test.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const path = require('../paths');
+const BaseControllerTest = require(`${path.TESTS}/baseControllerTest.js`);
+
+const ROUTE     = process.env.Route || '/depacomp-api';
+const VERSION   = process.env.VERSION || '/v1';
+
+const route = `${ROUTE}${VERSION}/documentoTipos`;
+const controllerName = 'DocumentoTipos';
+const singularName = 'DocumentoTipo';
+
+
+const generoControllerTest = new BaseControllerTest(route);
+generoControllerTest.runTests(controllerName, singularName, 'nombre');
