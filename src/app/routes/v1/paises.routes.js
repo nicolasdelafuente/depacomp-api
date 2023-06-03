c98399cea39325328c7b3f5f1503b8256cb0b16f
marianraven <mariangsdh@gmail.com>
2023-06-03 02:49:23 -0300

    Swagger para Paises, Provincias

--- src/app/routes/v1/paises.routes.js
+++ src/app/routes/v1/paises.routes.js
@@ -3,10 +3,286 @@ const express = require('express');
 const router = express.Router();
 const PaisController = require(`${path.CONTROLLERS}/PaisesControllers`);
 
-router.post('/', PaisController.create);
-router.get('/', PaisController.get);
-router.get('/:id', PaisController.getById);
+
+/**Schemas
+ * @swagger
+ * components:
+ *      schemas:
+ *          Paises:
+ *              required:
+ *                  - nombre
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                      format: int64
+ *                      example: 1
+ *                  nombre:
+ *                      type: string
+ *                      format: binary
+ *                      example: Afganistán 
+ *                  created_at:
+ *                      type: string
+ *                      example: 2023-02-21 23:47:20
+ *                  updated_at: 
+ *                      type: string
+ *                      example: 2023-02-21 23:47:20
+ *             
+ */
+
+// Hay que ver como se agrega con el schema bien hecho
+/**
+ * @swagger
+ * /paises:
+ *  post:
+ *      tags:
+ *          - Paises
+ *      summary: Agregar un nuevo Pais
+ *      description: Agregar un nuevo Pais
+ *      operationId: create
+ *      requestBody:
+ *          description: Agregar un nuevo Pais
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object 
+ *                      properties:
+ *                          nombre:
+ *                              type: string
+ *                              format: binary
+ *                              example: Afganistán
+ *                          pais_id:
+ *                              type: integer
+ *                              format: int64
+ *                              example: 1
+ *          required: true
+ *      responses:
+ *          '200':
+ *              description: Pais agregado con exito
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Paises'
+ *          '405':
+ *              description: Invalid input
+ */
+
+ router.post('/', PaisController.create);
+
+ /**
+  * @swagger
+  * /paises:
+  *  get:
+  *      tags:
+  *        - Paises
+  *      summary: Lista todos los Paises existentes
+  *      description: Lista todos los Paises existentes
+  *      operationId: get
+  *      content:
+  *          application/json:
+  *              schema:
+  *                  type: object
+  *              properties:
+  *                  status:
+  *                      type: string
+  *                      example: OK
+  *                  data:
+  *                      type: array 
+  *                      items: 
+  *                          $ref: "#/components/schemas/Paises"
+  *      responses:
+  *          '200':
+  *              description: Se listaron todos los Paises con exito
+  *              content:
+  *                  application/json:
+  *                      schema:
+  *                          type: object
+  *                          properties:
+  *                              status:
+  *                                  type: string
+  *                                  example: OK
+  *                              data:
+  *                                  type: array 
+  *                                  items: 
+  *                                      $ref: "#/components/schemas/Paises"
+  *          '404':
+  *              description: No se encontraron
+  *              content:
+  *                  application/json:
+  *                      schema:
+  *                          type: object
+  *                          properties:
+  *                              status:
+  *                                  type: string
+  *                                  example: NOT FOUND
+  *                              data:
+  *                                  type: object
+  *                                  properties:
+  *                                      error:
+  *                                          type: string 
+  *                                          example: "No se encontraron"
+  *          '500':
+  *              description: FAILED
+  *              content:
+  *                  application/json:
+  *                      schema:
+  *                          type: object
+  *                          properties:
+  *                              status:
+  *                                  type: string
+  *                                  example: FAILED
+  *                              data:
+  *                                  type: object
+  *                                  properties:
+  *                                      error:
+  *                                          type: string 
+  *                                          example: "FAILED"
+  */
+ 
+ router.get('/', PaisController.get);
+ 
+ /**
+  * @swagger
+  * /paises/{Id}:
+  *  get:
+  *     tags:
+  *       - Paises
+  *     summary: Buscar Pais por ID
+  *     description: Buscar Pais
+  *     operationId: getById
+  *     parameters:
+  *       - name: Id
+  *         in: path
+  *         description: ID de Pais a devolver
+  *         required: true
+  *         schema:
+  *             type: integer
+  *             format: int32
+  *     responses:
+  *          '200':
+  *              description: Se listo la Pais por ID
+  *              content:
+  *                  application/json:
+  *                      schema:
+  *                          type: object
+  *                          properties:
+  *                              status:
+  *                                  type: string
+  *                                  example: OK
+  *                              data:
+  *                                  type: array 
+  *                                  items: 
+  *                                      $ref: "#/components/schemas/Pais"
+  *          '404':
+  *              description: No se encontro el Pais por ID
+  *              content:
+  *                  application/json:
+  *                      schema:
+  *                          type: object
+  *                          properties:
+  *                              status:
+  *                                  type: string
+  *                                  example: NOT FOUND
+  *                              data:
+  *                                  type: object
+  *                                  properties:
+  *                                      error:
+  *                                          type: string 
+  *                                          example: "No se encontro"
+  *          '500':
+  *              description: FAILED
+  *              content:
+  *                  application/json:
+  *                      schema:
+  *                          type: object
+  *                          properties:
+  *                              status:
+  *                                  type: string
+  *                                  example: FAILED
+  *                              data:
+  *                                  type: object
+  *                                  properties:
+  *                                      error:
+  *                                          type: string 
+  *                                          example: "FAILED"
+  */
+ 
+ router.get('/:id', PaisController.getById);
+ 
+ /**
+  * @swagger
+  * /paises/{Id}:
+  *  put:
+  *      tags:
+  *          - Paises
+  *      summary: Update de un Pais existente
+  *      description: Actualizacion de una Pais por Id
+  *      operationId: update
+  *      parameters:
+  *        - name: Id
+  *          in: path
+  *          description: ID de un Pais para ser modficado
+  *          required: true
+  *          schema:
+  *              type: integer
+  *              format: int64
+  *      requestBody:
+  *          description: Actualizacion de un Pais por id
+  *          content:
+  *              application/json:
+  *                  schema:
+  *                      type: object 
+  *                      properties:
+  *                          nombre:
+  *                              type: string
+  *                              format: binary
+  *                              example: Buenos Aires
+  *                          pais_id:
+  *                              type: integer
+  *                              format: int64
+  *                              example: 1
+  *          required: true
+  *      responses:
+  *          '200':
+  *              description: Se actualizo el Pais existente
+  *              content:
+  *                  application/json:
+  *                      schema:
+  *                          $ref: '#/components/schemas/Pais'
+  *          '400':
+  *              description: El ID sumistrado es incorrecto
+  *          '404':
+  *              description: El Pais no existe
+  *          '405':
+  *              description: Validation exception
+  */
+ 
 router.put('/:id', PaisController.update);
+ 
+ /**
+  * @swagger
+  * /pais/{Id}:
+  *  delete:
+  *      tags:
+  *          - Paises
+  *      summary: Elimina un Pais por el id
+  *      description: Borra el Pais segun el id enviado
+  *      operationId: destroy
+  *      parameters:
+  *        - name: Id
+  *          in: path
+  *          description: ID del Pais para ser borrado
+  *          required: true
+  *          schema:
+  *              type: integer
+  *              format: int64
+  *      responses:
+  *          '204':
+  *              description: El Pais fue eliminada correctamente
+  *          '400':
+  *              description: El Pais es incorrecta
+  */
+ 
 router.delete('/:id', PaisController.destroy);
 
 module.exports = router;
--- src/app/routes/v1/provincias.routes.js
+++ src/app/routes/v1/provincias.routes.js
@@ -3,10 +3,287 @@ const express = require('express');
 const router = express.Router();
 const ProvinciaController = require(`${path.CONTROLLERS}/ProvinciasControllers`);
 
-router.post('/', ProvinciaController.create);
-router.get('/', ProvinciaController.get);
-router.get('/:id', ProvinciaController.getById);
+/**Schemas
+ * @swagger
+ * components:
+ *      schemas:
+ *          Provincias:
+ *              required:
+ *                  - nombre
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                      format: int64
+ *                      example: 1
+ *                  nombre:
+ *                      type: string
+ *                      format: binary
+ *                      example: Buenos Aires  
+ *                  created_at:
+ *                      type: string
+ *                      example: 2023-02-21 23:47:20
+ *                  updated_at: 
+ *                      type: string
+ *                      example: 2023-02-21 23:47:20
+ *                  Paises:
+ *                      $ref: '#/components/schemas/Paises'
+ */
+
+// Hay que ver como se agrega con el schema bien hecho
+/**
+ * @swagger
+ * /provincias:
+ *  post:
+ *      tags:
+ *          - Provincias
+ *      summary: Agregar una nueva provincia
+ *      description: Agregar una nueva provincia
+ *      operationId: create
+ *      requestBody:
+ *          description: Agregar una nueva provincia
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object 
+ *                      properties:
+ *                          nombre:
+ *                              type: string
+ *                              format: binary
+ *                              example: Buenos Aires
+ *                          pais_id:
+ *                              type: integer
+ *                              format: int64
+ *                              example: 1
+ *          required: true
+ *      responses:
+ *          '200':
+ *              description: Provincia agregada con exito
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Provincia'
+ *          '405':
+ *              description: Invalid input
+ */
+
+ router.post('/', ProvinciaController.create);
+
+ /**
+  * @swagger
+  * /provincias:
+  *  get:
+  *      tags:
+  *        - Provincias
+  *      summary: Lista todas las Provincias existentes
+  *      description: Lista todas las Provincias existentes
+  *      operationId: get
+  *      content:
+  *          application/json:
+  *              schema:
+  *                  type: object
+  *              properties:
+  *                  status:
+  *                      type: string
+  *                      example: OK
+  *                  data:
+  *                      type: array 
+  *                      items: 
+  *                          $ref: "#/components/schemas/Provincias"
+  *      responses:
+  *          '200':
+  *              description: Se listaron todas las Provincias con exito
+  *              content:
+  *                  application/json:
+  *                      schema:
+  *                          type: object
+  *                          properties:
+  *                              status:
+  *                                  type: string
+  *                                  example: OK
+  *                              data:
+  *                                  type: array 
+  *                                  items: 
+  *                                      $ref: "#/components/schemas/Provincias"
+  *          '404':
+  *              description: No se encontraron
+  *              content:
+  *                  application/json:
+  *                      schema:
+  *                          type: object
+  *                          properties:
+  *                              status:
+  *                                  type: string
+  *                                  example: NOT FOUND
+  *                              data:
+  *                                  type: object
+  *                                  properties:
+  *                                      error:
+  *                                          type: string 
+  *                                          example: "No se encontraron"
+  *          '500':
+  *              description: FAILED
+  *              content:
+  *                  application/json:
+  *                      schema:
+  *                          type: object
+  *                          properties:
+  *                              status:
+  *                                  type: string
+  *                                  example: FAILED
+  *                              data:
+  *                                  type: object
+  *                                  properties:
+  *                                      error:
+  *                                          type: string 
+  *                                          example: "FAILED"
+  */
+ 
+ router.get('/', ProvinciaController.get);
+ 
+ /**
+  * @swagger
+  * /provincias/{Id}:
+  *  get:
+  *     tags:
+  *       - Provincia
+  *     summary: Buscar Provincia por ID
+  *     description: Buscar Provincia
+  *     operationId: getById
+  *     parameters:
+  *       - name: Id
+  *         in: path
+  *         description: ID de Provincia a devolver
+  *         required: true
+  *         schema:
+  *             type: integer
+  *             format: int32
+  *     responses:
+  *          '200':
+  *              description: Se listo la provincia por ID
+  *              content:
+  *                  application/json:
+  *                      schema:
+  *                          type: object
+  *                          properties:
+  *                              status:
+  *                                  type: string
+  *                                  example: OK
+  *                              data:
+  *                                  type: array 
+  *                                  items: 
+  *                                      $ref: "#/components/schemas/Provincias"
+  *          '404':
+  *              description: No se encontro la carrera por ID
+  *              content:
+  *                  application/json:
+  *                      schema:
+  *                          type: object
+  *                          properties:
+  *                              status:
+  *                                  type: string
+  *                                  example: NOT FOUND
+  *                              data:
+  *                                  type: object
+  *                                  properties:
+  *                                      error:
+  *                                          type: string 
+  *                                          example: "No se encontro"
+  *          '500':
+  *              description: FAILED
+  *              content:
+  *                  application/json:
+  *                      schema:
+  *                          type: object
+  *                          properties:
+  *                              status:
+  *                                  type: string
+  *                                  example: FAILED
+  *                              data:
+  *                                  type: object
+  *                                  properties:
+  *                                      error:
+  *                                          type: string 
+  *                                          example: "FAILED"
+  */
+ 
+ router.get('/:id', ProvinciaController.getById);
+
+ 
+ /**
+  * @swagger
+  * /provincias/{Id}:
+  *  put:
+  *      tags:
+  *          - Provincias
+  *      summary: Update de una Provincia existente
+  *      description: Actualizacion de una Provincia por Id
+  *      operationId: update
+  *      parameters:
+  *        - name: Id
+  *          in: path
+  *          description: ID de una Provincia para ser modficado
+  *          required: true
+  *          schema:
+  *              type: integer
+  *              format: int64
+  *      requestBody:
+  *          description: Actualizacion de una Provincia por id
+  *          content:
+  *              application/json:
+  *                  schema:
+  *                      type: object 
+  *                      properties:
+  *                          nombre:
+  *                              type: string
+  *                              format: binary
+  *                              example: Buenos Aires
+  *                          pais_id:
+  *                              type: integer
+  *                              format: int64
+  *                              example: 1
+  *          required: true
+  *      responses:
+  *          '200':
+  *              description: Se actualizo la Provincia existente
+  *              content:
+  *                  application/json:
+  *                      schema:
+  *                          $ref: '#/components/schemas/Provincias'
+  *          '400':
+  *              description: El ID sumistrado es incorrecto
+  *          '404':
+  *              description: La Carrera no existe
+  *          '405':
+  *              description: Validation exception
+  */
+ 
 router.put('/:id', ProvinciaController.update);
+ 
+ /**
+  * @swagger
+  * /provincias/{Id}:
+  *  delete:
+  *      tags:
+  *          - Provincias
+  *      summary: Elimina una Provincia por el id
+  *      description: Borra la Provincia segun el id enviado
+  *      operationId: destroy
+  *      parameters:
+  *        - name: Id
+  *          in: path
+  *          description: ID de la Provincia para ser borrado
+  *          required: true
+  *          schema:
+  *              type: integer
+  *              format: int64
+  *      responses:
+  *          '204':
+  *              description: La Provincia fue eliminada correctamente
+  *          '400':
+  *              description: La Provincia es incorrecta
+  */
+ 
 router.delete('/:id', ProvinciaController.destroy);
 
 module.exports = router;
