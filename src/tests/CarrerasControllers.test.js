2059d432a5ef8cd57befcd8f8c80e8c2b06300a3
adr <adriana.amarilla@unahur.edu.ar>
2023-06-10 03:15:21 -0300

    Arreglo de test

--- src/tests/CarrerasControllers.test.js
+++ src/tests/CarrerasControllers.test.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 
-const path = require('../paths');
+const path = require('../../paths');
 const BaseControllerTest = require(`${path.TESTS}/baseControllerTest.js`);
 
 const ROUTE     = process.env.Route || '/depacomp-api';
 const VERSION   = process.env.VERSION || '/v1';
 
-const route = `${ROUTE}${VERSION}/paises`;
-const controllerName = 'Paises';
-const singularName = 'Pais';
+const route = `${ROUTE}${VERSION}/carreras`;
+const controllerName = 'Carreras';
+const singularName = 'Carrera';
 
 
 const generoControllerTest = new BaseControllerTest(route);
